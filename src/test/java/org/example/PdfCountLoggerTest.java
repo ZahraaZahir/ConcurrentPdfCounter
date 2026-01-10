3382143453809ed65ed4899b67368adf559732ee
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-01-10 15:17:09 +0300

    test: add unit tests for the pdf logger

---
+++ src/test/java/org/example/PdfCountLoggerTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PdfCountLoggerTest {
+
+    @Test
+    void should_StartWithZero_When_ScannerIsInitialized() {
+        Object monitor = new Object();
+        PdfCountLogger logger = new PdfCountLogger(monitor);
+        DirectoryScannerUsingThreadPool scanner = new DirectoryScannerUsingThreadPool("path", logger, monitor);
+
+        assertEquals(0, scanner.getPdfCount());
+    }
+
+    @Test
+    void should_IncrementCountByOne_When_NotifyIsCalledOnce() {
+        Object monitor = new Object();
+        PdfCountLogger logger = new PdfCountLogger(monitor);
+        DirectoryScannerUsingThreadPool scanner = new DirectoryScannerUsingThreadPool("path", logger, monitor);
+
+        scanner.incrementAndNotify("TestThread");
+
+        assertEquals(1, scanner.getPdfCount());
+    }
+}
